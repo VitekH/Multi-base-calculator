72f277bf61e4c09889ce1c4473e7d756789129c7
VitekH <89918239+VitekH@users.noreply.github.com>
2022-05-13 13:37:15 +0200

    13.5. po hodině Inf

--- myscripts.js
+++ myscripts.js
@@ -4,30 +4,73 @@ var pozice=0;
 var a=0;
 const div_abc = document.querySelector(".abc")
 document.getElementById("=").onclick = zmackl_rovna_se
+document.getElementById("C").onclick = zmackl_C
 document.querySelector("#submitsoustava").onclick = zmackl_submitsoustava
+document.querySelector("#mazat").onclick = zmackl_mazat
 var priklad = [];
 function zmackl_tlacitko(button){
 	console.log(button)
     priklad.push(button.id);
+    document.querySelector(".abc").innerHTML = priklad.join("");
+}
+function zmackl_C(){
+    priklad = [];
+    document.querySelector(".abc").innerHTML = priklad.join("");
+}
+function zmackl_mazat() {
+    if(priklad.length == 1) {
+        priklad.pop()
+        document.querySelector(".abc").innerHTML = "</br>"
+    }
+    else{
+        priklad.pop()
+        document.querySelector(".abc").innerHTML = priklad.join("");
+    }
 }
 function zmackl_rovna_se(){
-    plus = priklad.indexOf("+");
+    if(isNaN(priklad.indexOf("+"))==false) {
+        let znamenko = priklad.indexOf("+")
+    }
+    if(isNaN(priklad.indexOf("-"))==false) {
+        znamenko = priklad.indexOf("-")
+    }
+     if(isNaN(priklad.indexOf("÷"))==false) {
+        znamenko = priklad.indexOf("÷")
+    }
+    if(isNaN(priklad.indexOf("*"))==false) {
+        znamenko = priklad.indexOf("*")
+    }
     let n=0;
     let krok=0;
     let x=0;
-    for (pozice = plus-1; pozice >= 0; pozice--, n++){
+    for (pozice = znamenko-1; pozice >= 0; pozice--, n++){
         krok = priklad[pozice];
         x += krok*(soustava**n);
     }
     let y=0;
     krok = 0;
     n=0;
-    for (pozice = priklad.length-1; pozice > plus; pozice--, n++){
+    for (pozice = priklad.length-1; pozice > znamenko; pozice--, n++){
         krok = priklad[pozice];
         y += krok*(soustava**n);
     }
-    z=x+y
-	alert(z.toString(soustava));
+    if(isNaN(priklad.indexOf("+"))==false) {
+        z=x+y
+    }
+    if(isNaN(priklad.indexOf("-"))==false) {
+        z=x-y
+    }
+    if(isNaN(priklad.indexOf("÷"))==false) {
+        z=x/y
+    }
+    if(isNaN(priklad.indexOf("+"))==false) {
+        z=x*y
+    }     
+    
+    
+    priklad.push("=")
+    priklad.push(z.toString(soustava))
+    document.querySelector(".abc").innerHTML = priklad.join("");
     priklad = [];
 }
 function zmackl_submitsoustava(){
@@ -43,7 +86,8 @@ function zmackl_submitsoustava(){
 	for (let i = 0; i < soustava; i++){
 	puzzle += "<button type=\"button\" id="+i+" onclick=\"zmackl_tlacitko(this)\">"+i+"</button>"
 	}
-	document.querySelector(".buttons").innerHTML = puzzle+"<button type=\"button\" id=\"+\" onclick=\"zmackl_tlacitko(this)\">+</button>"
+	document.querySelector(".buttons").innerHTML = puzzle+"<button type=\"button\" id=\"+\" onclick=\"zmackl_tlacitko(this)\">+</button>"+"<button type=\"button\" id=\"-\" onclick=\"zmackl_tlacitko(this)\">-</button>"
+    +"<button type=\"button\" id=\"÷\" onclick=\"zmackl_tlacitko(this)\">÷</button>"+"<button type=\"button\" id=\"*\" onclick=\"zmackl_tlacitko(this)\">*</button>"
 	
 }
 
