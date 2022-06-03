e861216f3ea64f181a3dc3c9465fc80ece155ed4
VitekH <89918239+VitekH@users.noreply.github.com>
2022-06-03 15:50:25 +0200

    Nefunckni
    vice soustav nez 10, nefunckni toString()

--- myscripts.js
+++ myscripts.js
@@ -10,8 +10,8 @@ document.querySelector("#submitsoustava").onclick = zmackl_submitsoustava
 document.querySelector("#mazat").onclick = zmackl_mazat
 var priklad = [];
 function zmackl_tlacitko(button){
-	console.log(button)
-    priklad.push(button.id);
+	console.log(pismena_soustava.indexOf(button.id))
+    priklad.push(pismena_soustava.indexOf(button.id));
     document.querySelector(".abc").innerHTML = priklad.join("");
 }
 function zmackl_znamenko(button){
@@ -78,9 +78,8 @@ function zmackl_rovna_se(){
         var u = 1;
         let q = 0;
         for(var u = 1; u <= druhy-prvni-1; u++){
-            console.log("tady")
-            cinitel +=(priklad[druhy-u])*(soustava**q)
-            console.log(cinitel)
+            let w = priklad[druhy-u].toString();
+            cinitel +=(pismena_soustava.indexOf(w))*(soustava**q)
             q++
         }
         if(prvni == -1){
@@ -125,129 +124,22 @@ function zmackl_rovna_se(){
     }
     
     //console.log(cinitel.toString(soustava))
-
-    priklad.push(mezivysledek.toString(soustava))
+    serazeni = [];
+    console.log(mezivysledek)
+    let p = 0;
+    if (mezivysledek > 0){
+        while(mezivysledek != 0){
+            p = mezivysledek % soustava
+            serazeni.push(p)
+            mezivysledek = (mezivysledek - p)/soustava
+        }
+    }
+    priklad.push((serazeni.reverse().join('')))
     document.querySelector(".abc").innerHTML = priklad.join("");
     priklad = [];
 
 }
-function zmackl_rovna_se_bezprednosti(){
-    priklad.push("=")
-    var znamenka = [-1];
-    var mezivysledek = 0;
-    for(let j = 0; j < priklad.length; j++){
-        if(priklad[j] == "+") {
-            znamenka.push(j)
-        }
-        if(priklad[j] == "-") {
-            znamenka.push(j)
-        }
-        if(priklad[j] == "*") {
-            znamenka.push(j)
-        }
-        if(priklad[j] == "÷") {
-            znamenka.push(j)
-        }
-        if(priklad[j] == "="){
-            znamenka.push(j)
-        }
-        console.log(j)
-    }
-    //znamenka.push(priklad.length)
-    console.log(priklad)
-    console.log(znamenka)
-// priklad: 1,1,+,1,1,=
-// znamenka: -1,2,5
-//(5-2)-1
-    let cinitel = 0;
-    for(var w = 0; w < znamenka.length-1; w++){
-        
-        var prvni = znamenka[w];
-        var druhy = znamenka[w+1];
-        var u = 1;
-        let q = 0;
-        for(var u = 1; u <= druhy-prvni-1; u++){
-            console.log("tady")
-            cinitel +=(priklad[druhy-u])*(soustava**q)
-            console.log(cinitel)
-            q++
-        }
-        if(prvni == -1){
-            mezivysledek=cinitel;
-        }
-        else{
-            priklad[prvni]
-            switch (priklad[prvni]) {
-                case "+":
-                    mezivysledek += cinitel;
-                    break;
-                case "-":
-                    mezivysledek -= cinitel;
-                    break;
-                case "*":
-                    mezivysledek *= cinitel;
-                    break;
-                case "÷":
-                    mezivysledek /= cinitel;
-                    break;
-            
-                default:
-                    //chyba
-                    break;
-            }
-        }
-        cinitel = 0;
-    }
 
-    
-    //console.log(cinitel.toString(soustava))
-
-    priklad.push(mezivysledek.toString(soustava))
-    document.querySelector(".abc").innerHTML = priklad.join("");
-    priklad = [];
-
-}
-function nepouzivana(){
-    if(priklad.includes("+")==true) {
-        var znamenko = priklad.indexOf("+")
-    }
-    if(priklad.includes("-")==true) {
-        var znamenko = priklad.indexOf("-")
-    }
-    if(priklad.includes("÷")==true) {
-        var znamenko = priklad.indexOf("÷")
-    }
-    if(priklad.includes("*")==true) {
-        var znamenko = priklad.indexOf("*")
-    }
-    let n=0;
-    let krok=0;
-    let x=0;
-    for (pozice = znamenko-1; pozice >= 0; pozice--, n++){
-        krok = priklad[pozice];
-        x += krok*(soustava**n);
-    }
-    let y=0;
-    krok = 0;
-    n=0;
-    for (pozice = priklad.length-1; pozice > znamenko; pozice--, n++){
-        krok = priklad[pozice];
-        y += krok*(soustava**n);
-    }
-    if(priklad.includes("+")==true) {
-        var z=x+y
-    } else if(priklad.includes("-")==true) {
-        var z=x-y
-    } else if(priklad.includes("÷")==true) {
-        var z=x/y
-    } else if(priklad.includes("*")==true) {
-        var z=x*y
-    }
-    priklad.push("=")
-    priklad.push(z.toString(soustava))
-    document.querySelector(".abc").innerHTML = priklad.join("");
-    priklad = [];
-}
 function zmackl_submitsoustava(){
     soustava = document.getElementById("soustava").value;
     if (isNaN(soustava)){
@@ -259,10 +151,10 @@ function zmackl_submitsoustava(){
 	let puzzle = "<tr>"
     for (let i = 0; i < soustava; i++){
         if(i%4==0){
-            puzzle += "</tr>"+"<tr>"+"<td style=\"width:25%\">"+"<button type=\"button\" class=\"button\" style=\"width:100%\" id="+i+" onclick=\"zmackl_tlacitko(this)\">"+i+"</button>"+"</td>"
+            puzzle += "</tr>"+"<tr>"+"<td style=\"width:25%\">"+"<button type=\"button\" class=\"button\" style=\"width:100%\" id="+pismena_soustava[i]+" onclick=\"zmackl_tlacitko(this)\">"+pismena_soustava[i]+"</button>"+"</td>"
         }
         else{
-            puzzle += "<td style=\"width:25%\">"+"<button type=\"button\" class=\"button\" style=\"width:100%\" id="+i+" onclick=\"zmackl_tlacitko(this)\">"+i+"</button>"+"</td>"  
+            puzzle += "<td style=\"width:25%\">"+"<button type=\"button\" class=\"button\" style=\"width:100%\" id="+pismena_soustava[i]+" onclick=\"zmackl_tlacitko(this)\">"+pismena_soustava[i]+"</button>"+"</td>"  
         }
     }
     document.querySelector(".buttons").innerHTML = puzzle+"<tr><td style=\"width:25%\"><button type=\"button\" class=\"button\" style=\"width:100%\" id=\"+\" onclick=\"zmackl_znamenko(this)\">+</button></td><td style=\"width:25%\"><button type=\"button\" class=\"button\" style=\"width:100%\" id=\"-\" onclick=\"zmackl_znamenko(this)\">-</button></td><td style=\"width:25%\"><button type=\"button\" class=\"button\" style=\"width:100%\" id=\"*\" onclick=\"zmackl_znamenko(this)\">*</button></td><td style=\"width:25%\"><button type=\"button\" class=\"button\" style=\"width:100%\" id=\"÷\" onclick=\"zmackl_znamenko(this)\">÷</button></td></tr>"+"</tr>"
